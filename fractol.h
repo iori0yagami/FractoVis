19b6ee283d9f94b340532572d005eaa549ae385e
Salaheddine Badri <sabadri@e3r9p9.1337.ma>
2024-12-23 02:00:41 +0100

    54797978

--- fractol.h
+++ fractol.h
@@ -10,7 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-// include/fractol.h
+
+
+
 
 #ifndef FRACTOL_H
 # define FRACTOL_H
@@ -24,14 +26,73 @@
 # define HEIGHT 1080
 
 typedef struct s_data {
-    void    *img;
-    char    *addr;
-    int     bits_per_pixel;
-    int     line_length;
-    int     endian;
+    void    *mlx;           
+    void    *win;           
+    void    *img;           
+    char    *addr;         
+    int     bits_per_pixel; // Number of bits per pixel
+    int     line_length;    // Line length of the image
+    int     endian;         // Endianness of the image
+    double  zoom;           // Current zoom level
+    double  move_x;         // Current horizontal offset
+    double  move_y;         // Current vertical offset
+    double  cr;             // Real part of the constant for Julia set
+    double  ci;             // Imaginary part of the constant for Julia set
+    char    *type;          // Fractal type (e.g., "julia", "mandelbrot")
 }               t_data;
 
 void    my_mlx_pixel_put(t_data *data, int x, int y, int color);
-void    ft_error();
+void    generate_julia(t_data *data);
+void    generate_mandelbrot(t_data *data);
+void    ft_putstr_fd(char *s1, int fd);
+int     ft_strcmp(char *s1, char *s2);
+void    ft_error(const char *msg);
+int     key_hook(int keycode, t_data *data);
 
 #endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // include/fractol.h
+
+// #ifndef FRACTOL_H
+// # define FRACTOL_H
+
+// # include "mlx.h"
+// # include <stdlib.h>
+// # include <stdio.h>
+// # include <unistd.h>
+
+// # define WIDTH 1080
+// # define HEIGHT 1080
+
+// typedef struct s_data {
+//     void    *img;
+//     char    *addr;
+//     int     bits_per_pixel;
+//     int     line_length;
+//     double  zoom;
+//     double  move_x;
+//     double  move_y;
+//     double  cr;
+//     double  ci;
+//     int     endian;
+// }               t_data;
+
+// void    my_mlx_pixel_put(t_data *data, int x, int y, int color);
+// void    ft_error();
+
+// #endif
